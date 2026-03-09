59af19db4eda874818b8946a66b16fe55950cb1a
ApostolosG <apo.gouv@gmail.com>
2026-03-09 18:31:54 +0200

    chore: setup static export, tailwind v4 theme, and inter font

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Required for Static Site Generation (SSG)
+  images: {
+    unoptimized: true, // Required because GitHub Pages doesn't have a dedicated Image Server
+  },
 };
 
 export default nextConfig;
